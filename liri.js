311f43f28e132094fe0942665fbdc1aca4d8fba5
EP <edisonxpaynter@gmail.com>
2019-04-17 02:23:31 -0400

    OMDB and BandsinTown Working

--- liri.js
+++ liri.js
@@ -1,59 +1,66 @@
 require("dotenv").config();
 require("fs")
-require("axios")
+var axios = require("axios")
 
 var keys = require("./keys.js")
+var Spotify = require('node-spotify-api');
+ 
+var bandsintown = require('bandsintown')
+var omdb = require('omdb');
 var user = process.argv[2]
 var searchTerm = process.argv.slice(3).join(" ")
 
 console.log(user)    
 
-// var Spotify = require('node-spotify-api');
+if(user === "spotify-this-song"){
+var spotify = new Spotify(keys.spotify);
  
-// var spotify = new Spotify(keys.spotify);
- 
-// spotify.search({ type: 'track', query: searchTerm, limit: 1}, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-
-// console.log(data.tracks.items[0].album.artists[0].external_urls.spotify)
-// console.log(data.tracks.items[0].album.name)
-// console.log(data.tracks.items[0].name);
-// console.log(data.tracks.items[0].album.artists[0].name); 
-// });
-
-omdb.search('saw', function(err, movies) {
-  if(err) {
-      return console.error(err);
+spotify.search({ type: 'track', query: searchTerm, limit: 1}, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
   }
 
-  if(movies.length < 1) {
-      return console.log('No movies were found!');
-  }
+console.log(data.tracks.items[0].album.artists[0].external_urls.spotify)
+console.log(data.tracks.items[0].album.name)
+console.log(data.tracks.items[0].name);
+console.log(data.tracks.items[0].album.artists[0].name); 
+});
+}
 
-  movies.forEach(function(movie) {
-      console.log('%s (%d)', movie.title, movie.year);
+if (user === "concert-this"){
+bandsintown
+axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp")
+  .then(function(response) {
+    console.log(response.data[0].venue.name)
+    console.log(response.data[0].venue.city + ", " + response.data[0].venue.country)
+    console.log(response.data[0].datetime)
+
   });
+}
 
-  // Saw (2004)
-  // Saw II (2005)
-  // Saw III (2006)
-  // Saw IV (2007)
-  // ...
-});
 
-omdb.get({ title: searchTerm}, true, function(err, movie) {
-  if(err) {
-      return console.error(err);
+if(user === "movie-this"){
+
+var movieName = searchTerm;
+if (movieName === ""){
+  movieName = "Mr. Nobody"
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log(response.data.Title)
+    console.log("Release Year: " +  response.data.Year)
+    console.log(response.data.Rated)
+    console.log(response.data.Ratings[1].Value)
+    console.log(response.data.Country)
+    console.log(response.data.Language)
+    console.log(response.data.Plot)
+    console.log(response.data.Actors);
   }
+);
 
-  if(!movie) {
-      return console.log('Movie not found!');
-  }
-
-  console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
-  console.log(movie.plot);
-
-
-});
+}
